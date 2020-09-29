cf31dc19aaf809cc37a62befcab5df46c634680d
Marek Pazik <poczta@marekpazik.pl>
2020-09-29 22:24:33 +1000

    Add github primer

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@primer/components";
 import React from "react";
 
-export const App: React.FC = () => <div className="App">Hello world</div>;
+export const App: React.FC = () => (
+  <div className="App">
+    Hello world <Button>test</Button>
+  </div>
+);
